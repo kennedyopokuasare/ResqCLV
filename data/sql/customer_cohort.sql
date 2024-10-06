0b65d24d7303e3386314ea294c6e4fdabab631ce
Asare <opokuasarekennedy@gmail.com>
2024-10-06 13:44:18 +0300

    Fix a bug where historical customer cohorts are kept

--- data/sql/customer_cohort.sql
+++ data/sql/customer_cohort.sql
@@ -8,14 +8,20 @@ AS
             MIN(DATE(CREATEDAT)) AS FIRST_ORDER_DATE
         FROM ORDERS
         GROUP BY USERID
-    )
-    
-    SELECT 
-        O.USERID, 
-        O.CREATEDAT AS ORDER_DATE, 
-        FOD.FIRST_ORDER_DATE, 
-        ((strftime('%Y', O.CREATEDAT) - strftime('%Y', FOD.FIRST_ORDER_DATE)) * 12) + 
-        (strftime('%m', O.CREATEDAT) - strftime('%m', FOD.FIRST_ORDER_DATE)) AS COHORT
-    FROM ORDERS O
-    LEFT JOIN FIRSTORDERDATES FOD ON O.USERID = FOD.USERID
-    ORDER BY O.USERID, O.CREATEDAT;
+    ),
+    COHORTS AS (
+        SELECT 
+            USERID, 
+            MAX(COHORT) AS COHORT
+        FROM (
+                SELECT 
+                    O.USERID, 
+                    ((strftime('%Y', O.CREATEDAT) - strftime('%Y', FOD.FIRST_ORDER_DATE)) * 12) + 
+                    (strftime('%m', O.CREATEDAT) - strftime('%m', FOD.FIRST_ORDER_DATE)) AS COHORT
+                FROM ORDERS O
+                LEFT JOIN FIRSTORDERDATES FOD ON O.USERID = FOD.USERID
+        ) AS HISTORICALCOHORTS
+        GROUP BY USERID  
+)
+SELECT * FROM COHORTS 
+ORDER BY COHORT DESC;
